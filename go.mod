eb73e96dd21ff72d407fe363b69037a151764f64
capybara-translation <capybara.translation@gmail.com>
2026-08-31 22:01:41 +0900

    feat: add project skeleton with recv/send subcommand dispatch
    - initialize Go module github.com/capybara-translation/lanxfer
    - add main.go dispatching recv/send subcommands with usage and exit codes (2 = misuse, 1 = runtime error)
    - add .gitignore covering Go build outputs including the lanxfer binary

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/capybara-translation/lanxfer
+
+go 1.25.5
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		usage()
+		os.Exit(2)
+	}
+
+	var err error
+	switch os.Args[1] {
+	case "recv":
+		err = runRecv(os.Args[2:])
+	case "send":
+		err = runSend(os.Args[2:])
+	default:
+		fmt.Fprintf(os.Stderr, "lanxfer: unknown command %q\n", os.Args[1])
+		usage()
+		os.Exit(2)
+	}
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "lanxfer:", err)
+		os.Exit(1)
+	}
+}
+
+func usage() {
+	fmt.Fprintln(os.Stderr, `usage:
+  lanxfer recv [--dir <path>] [--port 8425] [--max-size <bytes>]
+  lanxfer send [--port 8425] <ip> <file>`)
+}
+
+// runRecv / runSend は後続タスクで実装する。
+func runRecv(args []string) error { return errors.New("recv: not implemented yet") }
+func runSend(args []string) error { return errors.New("send: not implemented yet") }
